2832596be0fe9d1ef6ba8987ecba11394c742784
takashi.akagi <nisyu@mac.com>
2013-11-22 18:21:19 +0900

    ブログにユーザーリスト追加

--- lib/acms_ruby/models/blog.rb
+++ lib/acms_ruby/models/blog.rb
@@ -4,12 +4,13 @@ module AcmsRuby
     scope :closes, -> { where(blog_status: :close) }
     scope :opens, -> { where(blog_status: :open) }
 
-    belongs_to :parent, class_name: 'Blog', foreign_key: :blog_parent, primary_key: :blog_id
+    belongs_to :parent, class_name: 'Blog', foreign_key: :blog_parent
 
-    has_many :categories, class_name: 'Category', foreign_key: :category_blog_id, primary_key: :blog_id
-    has_many :entries, class_name: 'Entry', foreign_key: :entry_blog_id, primary_key: :blog_id
-    has_many :fields, class_name: 'Field', foreign_key: :field_bid, primary_key: :blog_id
-    has_many :all_blog_fields, class_name: 'Field', foreign_key: :field_blog_id, primary_key: :blog_id
+    has_many :categories, class_name: 'Category', foreign_key: :category_blog_id
+    has_many :entries, class_name: 'Entry', foreign_key: :entry_blog_id
+    has_many :fields, class_name: 'Field', foreign_key: :field_bid
+    has_many :users, class_name: 'User', foreign_key: :user_blog_id
+    has_many :all_blog_fields, class_name: 'Field', foreign_key: :field_blog_id
 
     create_methods ['code','status','name','domain','generated_datetime'], 'blog'
   end
