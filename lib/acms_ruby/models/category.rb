c7caf0b20451d658193e49f31decdde25e8ecb90
takashi.akagi <nisyu@mac.com>
2013-11-22 16:58:04 +0900

    カテゴリ構造追加

--- lib/acms_ruby/models/category.rb
+++ lib/acms_ruby/models/category.rb
@@ -3,5 +3,12 @@ module AcmsRuby
   class Category < AcmsRuby::ARBase
     scope :closes, -> { where(entry_status: :close) }
     scope :opens, -> { where(entry_status: :open) }
+
+    belongs_to :parent, class_name: 'Category', foreign_key: :category_parent, primary_key: :category_id
+
+    has_many :categories, class_name: 'Category', foreign_key: :category_parent, primary_key: :category_id
+    has_many :entries, class_name: 'Entry', foreign_key: :entry_blog_id, primary_key: :blog_id
+
+    create_methods ['code','status','name'], 'category'
   end
 end
