a9914b9b1a2e995141389f67ef4a81fd95a66f0f
takashi.akagi <nisyu@mac.com>
2013-11-25 16:48:32 +0900

    カテゴリのグローバル設定に対応

--- lib/acms_ruby/models/blog.rb
+++ lib/acms_ruby/models/blog.rb
@@ -8,12 +8,23 @@ module AcmsRuby
 
     belongs_to :parent, class_name: 'Blog', foreign_key: :blog_parent
 
-    has_many :categories, class_name: 'Category', foreign_key: :category_blog_id
+    has_many :origin_categories, class_name: 'Category', foreign_key: :category_blog_id
     has_many :entries, class_name: 'Entry', foreign_key: :entry_blog_id
     has_many :fields, class_name: 'Field', foreign_key: :field_bid
     has_many :users, class_name: 'User', foreign_key: :user_blog_id
     has_many :all_blog_fields, class_name: 'Field', foreign_key: :field_blog_id
 
     create_methods ['code','status','name','domain','generated_datetime'], 'blog'
+
+    def categories
+      return origin_categories unless parent
+      parent.global_categories + origin_categories
+    end
+
+    def global_categories
+      g_cates = origin_categories.where category_scope: :global
+      return g_cates unless parent
+      parent.global_categories + g_cates
+    end
   end
 end
