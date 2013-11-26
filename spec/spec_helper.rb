643b876228d5ce2d11d12a5e4d05e6b7d71156e0
takashi.akagi <nisyu@mac.com>
2013-11-26 13:42:55 +0900

    DBのテスト前クリア

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,22 @@ require 'rubygems'
 require 'acms_ruby'
 require 'factory_girl'
 require 'factories'
+require 'database_cleaner'
 require 'rspec'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.establish_connection(config['test'])
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
