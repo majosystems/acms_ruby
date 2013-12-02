9c7aecdbe3144d121e8e2cc38298864c0adbf451
takashi.akagi <nisyu@mac.com>
2013-12-02 20:32:13 +0900

    フィールド部分だけを返そう・・・どうしよっかな

--- lib/acms_ruby/models/log_form.rb
+++ lib/acms_ruby/models/log_form.rb
@@ -8,7 +8,7 @@ module AcmsRuby
     create_methods ['datetime','remote_addr','user_agent','mail_to','mail_subject','mail_subject_admin','mail_body','mail_body_admin','data','form_id','entry_id','blog_id'], 'log_form'
 
     def fields
-      PHP.unserialize(data)
+      PHP.unserialize(data)[:_aryField]
     end
   end
 end
